700ba4c727f70951ed87e879c7327a67273074db
ilakosterin <ilya@in10.top>
2022-05-05 17:43:53 +0300

    Отредактировал README и начал писать скрипт программы

---
+++ main.py
@@ -0,0 +1,16 @@
+encryption_direction = int(input('Вы хотите зашифровать (0) или дешифровать (1)? (0/1) - '))
+lang = int(input('Выберите язык текста: Английский (0) или Русский (1) (0/1) - '))
+step = int(input('Введите шаг сдвига (цифра) - '))
+
+
+
+
+
+
+# k, s = int(input()), input()
+# n = 0
+# for x in range(len(s)):
+#     n = ord(s[x]) - k
+#     if n < 97:
+#         n = 122 - (96 - n)
+#     print(chr(n), end='')
